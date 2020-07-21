87f7c7c6a3ffd4958ba3c7cdf54f41af266bd026
Panny Lewis <pannylewis@gmail.com>
2020-07-20 21:17:26 -0500

    correct the initial value and the else if

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,18 +2,15 @@ import { useLocalStorage } from "../hooks/useLocalStorage";
 import { useEffect } from "react";
 
 export const useDarkMode = (key, initialValue) => {
+  const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
 
+  useEffect(() => {
+    if (darkMode === true) {
+      document.body.classList.add("dark-mode");
+    } else if (darkMode === false) {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
 
-  const [darkMode, setDarkMode] = useLocalStorage(key, value);
-
-    useEffect( ()  => {
-        if( darkMode  === true) {
-            document.body.classList.add(".dark-mode")
-        }
-        else(darkMode  === false) {
-          document.body.classList.remove(".dark-mode")
-        }
-    }, [darkMode])
-  
   return [darkMode, setDarkMode];
 };
